aa6ebeb9842b6e7140411342f7afec37a9fb938d
influxiq1 <himadribar.influxiq@gmail.com>
2020-11-27 12:05:28 +0530

    up-to-date for speaker list

--- src/app/speakeredit/speakeredit.component.ts
+++ src/app/speakeredit/speakeredit.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit , NgZone, EventEmitter } from '@angular/core';
+import { Component, OnInit, NgZone, EventEmitter } from '@angular/core';
 import { FormBuilder, Validators, FormControl, FormGroup } from '@angular/forms';
 import { Http } from '@angular/http';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import {Commonservices} from '../app.commonservices' ;
+import { Commonservices } from '../app.commonservices';
 import { UploadOutput, UploadInput, UploadFile, humanizeBytes, UploaderOptions } from 'ngx-uploader';
 declare var moment: any;
 
 @Component({
-  selector: 'app-speakeredit',
-  templateUrl: './speakeredit.component.html',
-  styleUrls: ['./speakeredit.component.css'],
+    selector: 'app-speakeredit',
+    templateUrl: './speakeredit.component.html',
+    styleUrls: ['./speakeredit.component.css'],
     providers: [Commonservices],
 })
 export class SpeakereditComponent implements OnInit {
@@ -49,7 +49,7 @@ export class SpeakereditComponent implements OnInit {
     public errckeditor;
     public ErrCode: boolean = false;
     public imageErrCode: boolean = false;
-    public fullimg:any = {};
+    public fullimg: any = {};
 
     constructor(fb: FormBuilder, private _http: Http, private router: Router, private route: ActivatedRoute, private _commonservices: Commonservices) {
         this.fb = fb;
@@ -71,16 +71,16 @@ export class SpeakereditComponent implements OnInit {
             description: [''],
             subjects_covered: [''],
             start_date_time: [''],
-        /*    end_date_time: [''],*/
+            /*    end_date_time: [''],*/
             year_2018: [''],
             year_2019: [''],
             year_2020: [''],
             year_2021: ['']
         });
 
-        console.log("aaaaaaaaaaaaa", this.dataForm );
+        console.log("aaaaaaaaaaaaa", this.dataForm);
 
-        this.zone = new NgZone({enableLongStackTrace: false});
+        this.zone = new NgZone({ enableLongStackTrace: false });
         this.basicOptions = {
             url: this.serverurl + 'uploads',
             filterExtensions: false,
@@ -91,16 +91,16 @@ export class SpeakereditComponent implements OnInit {
 
     getdetailsoffile() {
         let link = this.serverurl + 'speakerdetails';
-        let data = {id : this.id};
+        let data = { id: this.id };
         this._http.post(link, data)
             .subscribe(res => {
                 let result = res.json();
                 // console.log('result is  -');
                 // console.log(result);
                 // console.log(result.status);
-                if (result.status == 'success' && typeof(result) != 'undefined') {
+                if (result.status == 'success' && typeof (result) != 'undefined') {
                     let userdet = result.res[0];
-                    console.log('------------',userdet);
+                    console.log('------------', userdet);
                     // console.log('cccccccccccccccc',userdet);
                     // console.log(JSON.parse(userdet.subjects_covered));
                     // this.parsefile = JSON.parse(userdet.servername);
@@ -124,34 +124,34 @@ export class SpeakereditComponent implements OnInit {
                     // let inverseOffset = moment(timestamp).utcOffset() * -1;
                     // userdet.start_date_time = moment().utcOffset( inverseOffset  );
 
-                    let year_2018_put ;
-                    let year_2019_put ;
-                    let year_2020_put ;
-                    let year_2021_put ;
-                    if(userdet.year_2018==1){
+                    let year_2018_put;
+                    let year_2019_put;
+                    let year_2020_put;
+                    let year_2021_put;
+                    if (userdet.year_2018 == 1) {
                         year_2018_put = true;
-                    }else{
+                    } else {
                         year_2018_put = false;
                     }
-                    if(userdet.year_2019==1){
+                    if (userdet.year_2019 == 1) {
                         year_2019_put = true;
-                    }else{
+                    } else {
                         year_2019_put = false;
                     }
-                    if(userdet.year_2020==1){
+                    if (userdet.year_2020 == 1) {
                         year_2020_put = true;
-                    }else{
+                    } else {
                         year_2020_put = false;
                     }
 
-                    if(userdet.year_2021==1){
+                    if (userdet.year_2021 == 1) {
                         year_2021_put = true;
-                    }else{
+                    } else {
                         year_2021_put = false;
                     }
                     this.ckeditorContent = userdet.description;
                     this.dataForm = this.fb.group({
-                        speaker_name:[userdet.speaker_name],
+                        speaker_name: [userdet.speaker_name],
 
                         description: [userdet.description],
                         subjects_covered: [userdet.subjects_covered],
@@ -163,14 +163,14 @@ export class SpeakereditComponent implements OnInit {
                         year_2021: year_2021_put,
 
                     });
-                   (<FormControl>this.dataForm.controls['speaker_name']).setValue(userdet.speaker_name);
+                    (<FormControl>this.dataForm.controls['speaker_name']).setValue(userdet.speaker_name);
                     var subjects_coveredJoin = userdet.subjects_covered.join();
                     (<FormControl>this.dataForm.controls['subjects_covered']).setValue(subjects_coveredJoin);
                     (<FormControl>this.dataForm.controls['start_date_time']).setValue(moment(userdet.start_date_time).format());
                     this.fullimg = userdet.image;
                     console.log(this.configData)
-                }else {
-                     this.router.navigate(['/speakerlist']);
+                } else {
+                    this.router.navigate(['/speakerlist']);
                 }
             }, error => {
                 console.log('Ooops');
@@ -186,38 +186,54 @@ export class SpeakereditComponent implements OnInit {
         if (this.configData.files) {
 
             if (this.configData.files.length > 1) { this.ErrCode = true; return; }
-            formval.inventory_image =
+                formval.inventory_image =
                 {
                     "basepath": this.configData.files[0].upload.data.basepath,
                     "image": this.configData.files[0].upload.data.data.fileservername,
                     "name": this.configData.files[0].name,
                     "type": this.configData.files[0].type
                 };
-                // console.log('+++++++++++',typeof(formval.inventory_image.basepath));
-        } else {
+                console.log('=======================', formval.inventory_image)
+          
+            // console.log('+++++++++++',typeof(formval.inventory_image.basepath));
+        }
+        if (this.fullimg != null && this.fullimg != '' && !this.configData.files) {
+
+            formval.inventory_image =
+            {
+                "basepath": this.fullimg.basepath,
+                "image":  this.fullimg.image,
+                "name": this.fullimg.name,
+                "type": this.fullimg.type
+            };
+
+            console.log('--------------------------------', formval.inventory_image)
+
+        }
+        if(formval.inventory_image == null || formval.inventory_image == '' || formval.inventory_image == undefined ) {
             formval.inventory_image = false;
             this.imageErrCode = true;
             // if (this.action == 'edit')
-                // this.addinventorylistingquoteForm.value.inventory_image = this.defaultData.inventory_image;
+            // this.addinventorylistingquoteForm.value.inventory_image = this.defaultData.inventory_image;
         }
 
 
 
 
 
-      formval.start_date_time = moment(formval.start_date_time).format('YYYY-MM-DD HH:mm:ss');
-      //   console.log(formval.subjects_covered);
-      //   console.log(formval.subjects_covered);
-      //   var subjects_covered_array = formval.subjects_covered;
-      var subjects_covered_array = formval.subjects_covered.split(',');
+        formval.start_date_time = moment(formval.start_date_time).format('YYYY-MM-DD HH:mm:ss');
+        //   console.log(formval.subjects_covered);
+        //   console.log(formval.subjects_covered);
+        //   var subjects_covered_array = formval.subjects_covered;
+        var subjects_covered_array = formval.subjects_covered.split(',');
 
-      console.log('zxhgfshgfsahfsahdfsahday', subjects_covered_array);
+        console.log('zxhgfshgfsahfsahdfsahday', subjects_covered_array);
 
-        let file= this.files;
+        let file = this.files;
         console.log(file);
         let sfilename = [];
         let uploadedfilename = [];
-        if (typeof(file) == 'object') {  // newly added images
+        if (typeof (file) == 'object') {  // newly added images
             for (let i in file) {
                 sfilename.push(file[i].response);
                 uploadedfilename.push(file[i].name);
@@ -237,72 +253,72 @@ export class SpeakereditComponent implements OnInit {
         }
         console.log('uploadedfilename---------');
         console.log(uploadedfilename);
-       /* if (formval.status == true) {
-            formval.status = 1;
-        }
-        if (formval.status == false) {
-            formval.status = 0;
-        }*/
+        /* if (formval.status == true) {
+             formval.status = 1;
+         }
+         if (formval.status == false) {
+             formval.status = 0;
+         }*/
         let l_year_2018;
         let l_year_2019;
         let l_year_2020;
         let l_year_2021;
-        if(formval.year_2018==true){
-            l_year_2018=1;
-        }else{
+        if (formval.year_2018 == true) {
+            l_year_2018 = 1;
+        } else {
             l_year_2018 = 0;
         }
-        if(formval.year_2019==true){
-            l_year_2019=1;
-        }else{
+        if (formval.year_2019 == true) {
+            l_year_2019 = 1;
+        } else {
             l_year_2019 = 0;
         }
 
-        if(formval.year_2020==true){
-            l_year_2020=1;
-        }else{
+        if (formval.year_2020 == true) {
+            l_year_2020 = 1;
+        } else {
             l_year_2020 = 0;
         }
 
-        if(formval.year_2021==true){
-            l_year_2021=1;
-        }else{
+        if (formval.year_2021 == true) {
+            l_year_2021 = 1;
+        } else {
             l_year_2021 = 0;
         }
         console.log(this.dataForm.valid);
 
 
-      //  if (this.dataForm.valid) {
-            let link= this.serverurl + 'editspeaker';
-            let data = {
-                id: this.id,
-                speaker_name: formval.speaker_name,
-                description: this.ckeditorContent,
-                servername: JSON.stringify(sfilename),
-                uploadedfilename: JSON.stringify(uploadedfilename),
-                subjects_covered: subjects_covered_array,
-               start_date_time: formval.start_date_time,
-              /*  end_date_time: formval.end_date_time,*/
-                year_2018: l_year_2018,
-                year_2019: l_year_2019,
-                year_2020: l_year_2020,
-                year_2021: l_year_2021,
-                image:formval.inventory_image
-            };
-            console.log(data);
-            this._http.post(link, data)
-                .subscribe(data => {
-                    this.router.navigate(['/speakerlist']);
-                }, error => {
-                    console.log('Oooops!');
-                });
-       // }
+        //  if (this.dataForm.valid) {
+        let link = this.serverurl + 'editspeaker';
+        let data = {
+            id: this.id,
+            speaker_name: formval.speaker_name,
+            description: this.ckeditorContent,
+            servername: JSON.stringify(sfilename),
+            uploadedfilename: JSON.stringify(uploadedfilename),
+            subjects_covered: subjects_covered_array,
+            start_date_time: formval.start_date_time,
+            /*  end_date_time: formval.end_date_time,*/
+            year_2018: l_year_2018,
+            year_2019: l_year_2019,
+            year_2020: l_year_2020,
+            year_2021: l_year_2021,
+            image: formval.inventory_image
+        };
+        console.log(data);
+        this._http.post(link, data)
+            .subscribe(data => {
+                this.router.navigate(['/speakerlist']);
+            }, error => {
+                console.log('Oooops!');
+            });
+        // }
     }
 
 
     callit(filename) {
         console.log(filename);
-        if (filename == null || filename=='') {
+        if (filename == null || filename == '') {
             return '';
         }
         else {
@@ -313,78 +329,7 @@ export class SpeakereditComponent implements OnInit {
         }
     }
 
-    // onUploadOutput(output: UploadOutput): void {
-    //     setTimeout(()=> {
-    //         // alert(8);
-    //         if (output.type === 'allAddedToQueue') { // when all files added in queue
-    //             // uncomment this if you want to auto upload files when added
-    //             this.disableuploader = 1;
-    //             console.log('this.disableuploader === before');
-    //             console.log(this.disableuploader);
-    //             //  setTimeout(()=> {
-    //             const event: UploadInput = {
-    //                 type: 'uploadAll',
-    //                 url: this.serverurl + 'uploads',
-    //                 method: 'POST',
-    //             };
-    //             this.uploadInput.emit(event);
-    //             //   },200);
-    //         } else if (output.type === 'addedToQueue'  && typeof output.file !== 'undefined') { // add file to array when added
-    //             setTimeout(()=> {    // <<<---    using ()=> syntax
-    //                 console.log('output.file[0].response');
-    //                 console.log(output.file.response);
-    //                 console.log(output.file);
-    //                 console.log(output.file);
-    //                 // this.files.push(output.file);
-    //                 if(output.file.response!="") {
-    //                     // alert(7);
-    //                     console.log('output.file-------------------');
-    //                     console.log(output.file);
-    //                     console.log(output.file.response);
-    //                     // console.log(output.file[0].response);
-    //                     this.files=[];
-    //                     this.files.push(output.file);
-    //                 }
-    //                 this.disableuploader = 0;
-    //                 console.log('this.disableuploader after');
-    //                 console.log(this.disableuploader);
-    //                 // alert(22);
-    //                 // console.log(this.files);
-    //                 console.log('this.files');
-    //                 console.log(this.files);
-    //                 // alert(55);
-    //                 //  console.log(output.file[0].response);
-    //             },300);
-    //         } else if (output.type === 'uploading' && typeof output.file !== 'undefined') {
-    //             // alert(9);
-    //             console.log(this.files);
-    //             // update current data in files array for uploading file
-    //             const index = this.files.findIndex(file => typeof output.file !== 'undefined' && file.id === output.file.id);
-    //             this.files[index] = output.file;
-    //         } else if (output.type === 'removed') {
-    //             // remove file from array when removed
-    //             this.files = this.files.filter((file: UploadFile) => file !== output.file);
-    //         } else if (output.type === 'dragOver') {
-    //             this.dragOver = true;
-    //         } else if (output.type === 'dragOut') {
-    //             this.dragOver = false;
-    //         } else if (output.type === 'drop') {
-    //             this.dragOver = false;
-    //         }
-    //         this.dataForm.patchValue({servername: this.files});
-    //     },200);
-    // }
-
-    // startUpload(): void {
-    //     const event: UploadInput = {
-    //         type: 'uploadAll',
-    //         url: 'http://ngx-uploader.com/upload',
-    //         method: 'POST',
-    //         data: { foo: 'bar' }
-    //     };
-    //     this.uploadInput.emit(event);
-    // }
     deleteimage1(counter: any) {
-        this.files.splice(counter,1);
+        this.files.splice(counter, 1);
     }
 }
